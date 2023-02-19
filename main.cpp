61e18497bc9a65a449ff5e4d63796c3a351b395c
kudr.max <mdkudryashov@edu.hse.ru>
2023-02-19 13:59:45 +0300

    Initial init and added print_results() function

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "dijkstra.h"
+#include <iostream>
+
+int main() {
+    graph::Graph<std::string, int, double> gr;
+    gr.insert_node("one", 1);
+    gr.insert_node("two", 2);
+    gr.insert_node("three", 3);
+    gr.insert_node("four", 4);
+
+    gr.insert_edge(std::make_pair("one", "two"), 12);
+    gr.insert_edge(std::make_pair("one", "three"), 13);
+    gr.insert_edge(std::make_pair("four", "two"), 42);
+    gr.insert_edge(std::make_pair("two", "four"), 24);
+    gr.insert_edge(std::make_pair("two", "two"), 24);
+
+    gr.print();
+
+    double weight = 3.14;
+    std::vector<size_t> route = { 0, 3, 2, 5 };
+    dijkstra::print_results(weight, route);
+    return 0;
+}
